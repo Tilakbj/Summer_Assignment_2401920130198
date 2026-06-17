cc06846dc769175db8cf4a1e3f7b09677b16b58f
Tilakbj <tilakbhardwaj01@gmail.com>
2026-06-18 00:19:28 +0530

    Added Week_3 Day_3 solutions

---
+++ Week_3/Day_3/MinStack.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+public:
+stack<pair<int,int>>st;
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        if(st.empty()) st.push({value,value});
+        else{
+            st.push({value,min(value,st.top().second)});
+        }       
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
---
+++ Week_3/Day_3/ValidParenthesis.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n= s.size();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+            st.push(s[i]);
+
+            else{
+                if(st.empty()) return 0;
+
+                else if(s[i]==')'){
+                    if(st.top()!='(') return 0;
+                    else st.pop();
+                }
+                 else if(s[i]=='}'){
+                    if(st.top()!='{') return 0;
+                    else st.pop();
+                }
+                else{
+                    if(st.top()!='[') return 0;
+                    else st.pop();
+                }
+                
+            }
+        }
+        return st.empty();
+    }
+};
